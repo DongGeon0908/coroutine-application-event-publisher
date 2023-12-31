508a1bfa2046cfc75f601a9b094e9d0b20d47572
극락코딩 <50691225+DongGeon0908@users.noreply.github.com>
2023-12-31 17:15:38 +0900

    feat: event publisher

--- src/main/kotlin/com/goofy/event/pubsub/Event.kt
+++ src/main/kotlin/com/goofy/event/pubsub/Event.kt
@@ -1,9 +1,42 @@
 package com.goofy.event.pubsub
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import org.springframework.context.ApplicationEventPublisher
+import org.springframework.stereotype.Component
 import java.time.LocalDateTime
 
 sealed class Event<EVENT_DATA>(
+    val event: String,
     val data: EVENT_DATA,
     val publishedAt: LocalDateTime = LocalDateTime.now()
 )
 
+@Component
+class EventPublisher(
+    private val applicationEventPublisher: ApplicationEventPublisher
+) {
+    fun <EVENT_DATA> publish(event: Event<EVENT_DATA>) {
+        applicationEventPublisher.publishEvent(event)
+    }
+
+
+    fun <EVENT_DATA> multiPublish(events: List<Event<EVENT_DATA>>) {
+        events.forEach { event -> publish(event) }
+    }
+
+    suspend fun <EVENT_DATA> publishWithContext(event: Event<EVENT_DATA>) {
+        CoroutineScope(Dispatchers.IO).launch {
+            publish(event)
+        }
+    }
+
+    suspend fun <EVENT_DATA> multiPublishWithContext(events: List<Event<EVENT_DATA>>) {
+        events.forEach { event ->
+            CoroutineScope(Dispatchers.IO).launch {
+                publish(event)
+            }
+        }
+    }
+}
